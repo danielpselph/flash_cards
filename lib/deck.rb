31b8b82c8efff27d8b95f8cc4f03f7fdbaad39d7
Daniel Selph <daniel.p.selph@gmail.com>
2019-11-20 14:21:49 -0700

    Initial commit

---
+++ lib/deck.rb
@@ -0,0 +1,17 @@
+class Deck
+  attr_reader :cards
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def count
+    @deck.cards
+  end
+
+  def cards_in_category
+    
+  end
+
+
+end
---
+++ test/turn_test.rb
@@ -0,0 +1,40 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/turn'
+require 'pry'
+
+class TurnTest < Minitest::Test
+
+  def test_it_has_a_card
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    assert_instance_of Card, card
+  end
+
+  def test_it_has_a_turn
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    assert_instance_of Turn, turn
+  end
+
+  def test_it_has_a_guess
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    # binding.pry
+    assert_equal "Juneau", turn.guess
+  end
+
+  def test_if_correct
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    assert_equal true, turn.correct?
+  end
+
+  def test_it_has_feedback
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    binding.pry
+    assert_equal "Correct!", turn.feedback
+  end
+
+end
