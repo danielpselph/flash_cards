6e0acf4378ba58a37716abfc717febf68a275c61
Daniel Selph <daniel.p.selph@gmail.com>
2019-11-26 08:57:17 -0700

    Continued work on Round test and file, working on take_turn method

--- lib/round.rb
+++ lib/round.rb
@@ -1,19 +1,20 @@
 class Round
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :current_card
 
   def initialize(deck)
     @deck = deck
     @turns = []
+    @current_card = deck.cards[0]
   end
 
-  def current_card
-    @deck.cards[0]
-  end
+  # def current_card
+  #   @deck.cards[0]
+  # end
 
   def take_turn(guess)
-    new_turn = Turn.new(guess)
-    current_card += 1
-
+    # require "pry"; binding.pry
+    new_turn = Turn.new(current_card, guess)
+    @current_card += 1
   end
 
 
--- test/round_test.rb
+++ test/round_test.rb
@@ -12,7 +12,7 @@ class RoundTest < Minitest::Test
     @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    @deck = Deck.new([@card_1, @card_2, @card3])
+    @deck = Deck.new([@card_1, @card_2, @card_3])
     @round = Round.new(@deck)
   end
 
@@ -36,12 +36,19 @@ class RoundTest < Minitest::Test
     assert_equal @card_1, @round.current_card
   end
 
-  def test_can_take_turn
-      result = @round.take_turn("Juneau")
-    assert_instance_of Turn, result #check result.guess is "Juneau"
-      # @round.current_card
-    assert_equal @card_2, @round.current_card
+  def test_take_turn_changes_current_card
+    current_card = @round.current_card
+    @round.take_turn("string")
+    new_card = @round.current_card
+    assert_not_equal current_card, new_card
+    # binding.pry
+
+    #   result = @round.take_turn("Juneau")
+    # assert_instance_of Turn, result #check result.guess is "Juneau"
+    #   # @round.current_card
+    # assert_equal @card_2, @round.current_card
   end
+
 
 
 end
